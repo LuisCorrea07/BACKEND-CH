a7584349fd79f4568f5035773be7a57318b44944
LuisCorrea07 <luiscorrea070192@gmail.com>
2023-12-06 12:24:16 -0300

    new file:   .hintrc 	modified:   src/app.js 	modified:   src/public/newPet.html 	modified:   src/routes/views.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,33 +1,40 @@
-import { Router } from 'express'
+import { Router } from "express";
 
- 
-
-const router = Router()
-
- 
+const router = Router();
 
 const users = [
+  {
+    firstName: "Carlos",
+    lastName: "Perren",
+    age: 48,
+    email: "cperren@gmail.com",
+    phone: "+5493492222333",
+    role: "admin",
+  },
 
-    { firstName: 'Carlos', lastName: 'Perren', age: 48, email: 'cperren@gmail.com', phone: '+5493492222333', role: 'admin' },
+  {
+    firstName: "Carolina",
+    lastName: "Ferrero",
+    age: 42,
+    email: "cferrero@gmail.com",
+    phone: "+5493492222334",
+    role: "user",
+  },
 
-    { firstName: 'Carolina', lastName: 'Ferrero', age: 42, email: 'cferrero@gmail.com', phone: '+5493492222334', role: 'user' },
+  {
+    firstName: "Juan",
+    lastName: "Perez",
+    age: 30,
+    email: "jperez@gmail.com",
+    phone: "+5493492222335",
+    role: "user",
+  },
+];
 
-    { firstName: 'Juan', lastName: 'Perez', age: 30, email: 'jperez@gmail.com', phone: '+5493492222335', role: 'user' }
+router.get("/chat", (req, res) => {
+  res.render("chat", {
+    title: "Coder Compras Chat",
+  });
+});
 
-]
-
- 
-
-router.get('/chat', (req, res) => {
-
-    res.render('chat', {
-
-        title: 'Coder Compras Chat'
-
-    })
-
-})
-
- 
-
-export default router
+export default router;
