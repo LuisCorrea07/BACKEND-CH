a20ff9b618329dcbc04465065772e7d8fc1359d4
LuisCorrea07 <luiscorrea070192@gmail.com>
2023-12-19 19:13:49 -0300

    modified:   .gitignore 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const PORT = 5000;
+const PORT = 3000;
 const MONGOOSE_URL = 'mongodb://127.0.0.1:27017/ecommerce'
 
 const app = express();
@@ -17,49 +17,43 @@ app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-const httpServer = app.listen(PORT, () => {
-  console.log(`Servicio activo en puerto ${PORT}`);
-});
+(async () => {
+  try {
+    await mongoose.connect(MONGOOSE_URL);
+    const httpServer = app.listen(PORT, () => {
+      console.log(`Servicio activo en puerto ${PORT}`);
+    });
 
-const chat_messages = [];
+    const chat_messages = [];
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
-    credentials: false,
-  },
-});
+    const io = new Server(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+        credentials: false,
+      },
+    });
 
-io.on("connection", (socket) => {
-  socket.on("user_connected", (data) => {
-    socket.broadcast.emit("user_connected", data);
-  });
-});
+    io.on("connection", (socket) => {
+      socket.on("user_connected", (data) => {
+        socket.broadcast.emit("user_connected", data);
+      });
 
-io.on("connection", (socket) => {
-  socket.on("message", (data) => {
-    chat_messages.push(data);
-    io.emit("messageLogs", chat_messages);
-  });
-});
+      socket.on("message", (data) => {
+        // Consider storing a limited history of messages instead of pushing all messages
+        chat_messages.push(data);
+        io.emit("messageLogs", chat_messages);
+      });
+    });
 
-//Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.engine("handlebars", handlebars.engine());
-app.set("views", `${__dirname}/views`);
-app.set("view engine", "handlebars");
-app.set("io", io);
-app.use("/", viewsRouter);
-app.use("/static", express.static(`${__dirname}/public`));
-app.use("/users", usersRoutes);
-
-try {
-  await mongoose.connect(MONGOOSE_URL)
-  app.listen(PORT, () => {
-    console.log(`Servicio activo en puerto ${PORT}`)
-  })
-} catch (error) {
-  console.log(`Error al inicialiar servidor (${error.message})`)
-}
+    // Middlewares
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.set("io", io);
+    app.use("/", viewsRouter);
+    app.use("/static", express.static(`${__dirname}/public`));
+    app.use("/users", usersRoutes);
+  } catch (error) {
+    console.log(`Error al inicialiar servidor (${error.message})`);
+  }
+})();
