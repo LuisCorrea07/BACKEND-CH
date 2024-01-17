9a935aa3cbae09de3501b4775036a9305d3dcaf6
LuisCorrea07 <luiscorrea070192@gmail.com>
2024-01-16 21:45:54 -0300

    modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app.js 	new file:   src/routes/cookies.routes.js

---
+++ src/routes/cookies.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.get("/getcookies", async (req, res) => {
+  
+});
+
+router.get("/setcookies", async (req, res) => {
+  
+});
+
+router.get("/deletecookies", async (req, res) => {
+  
+});
+
+
+export default router;
